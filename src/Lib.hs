c687b3f903ca360bf1e2b5200b6a81be05af0725
Dom Meudec <>
2020-12-16 12:55:29 +0000

    Connected to API user successfully, messy JSON
    Was able to call my personal user account, dommeudec. Return failed

--- src/Lib.hs
+++ src/Lib.hs
@@ -16,9 +16,9 @@ module Lib
 
 import qualified GitHub as GH
 import qualified Servant.Client               as SC
-import           Network.HTTP.Client          (defaultManagerSettings,
-                                               newManager)
-                 
+import           Network.HTTP.Client          (newManager)
+import           Network.HTTP.Client.TLS      (tlsManagerSettings)
+
 someFunc :: IO ()
 someFunc = do
   putStrLn "Let's try a GitHubCall"
@@ -27,16 +27,16 @@ someFunc = do
 
 
 testGitHubCall :: IO ()
-testGitHubCall = 
-  (SC.runClientM GH.test =<< env) >>= \case
+testGitHubCall =
+  (SC.runClientM (GH.test (Just "haskell-app") "dommeudec") =<< env) >>= \case
 
     Left err -> do
       putStrLn $ "heuston, we have a problem: " ++ show err
     Right res -> do
       putStrLn $ "the votes of the github jury are " ++ show res
-      
+
 
   where env :: IO SC.ClientEnv
         env = do
-          manager <- newManager defaultManagerSettings
+          manager <- newManager tlsManagerSettings
           return $ SC.mkClientEnv manager (SC.BaseUrl SC.Http "api.github.com" 80 "")
